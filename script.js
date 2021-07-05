1bf796b764428a58f72d5196002a253a02494bff
Aditaya Burnwal <aditaya189@gmail.com>
2021-07-05 17:24:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+function _(selector){
+  return document.querySelector(selector);
+}
+function setup(){
+  let canvas = createCanvas(1536, 728);
+  canvas.parent("canvas-wrapper");
+   defaultbg();
+}
+
+// function myFunction() {
+  //   document.body.style.backgroundColor = "red";
+// }
+// function myFunction2() {
+//   document.body.style.backgroundColor = "white";
+// }
+function mouseDragged(){
+  let type = _("#pen-pencil").checked?"pencil":"brush";
+  // let method =_("#pen-eraser").checked?"eraser":"pencil";
+  let size = parseInt(_("#pen-size").value);
+  let color = _("#pen-color").value;
+  fill(color);
+  stroke(color);
+  if(type == "pencil"){
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    white();
+  } 
+  // else if(method == "eraser"){
+  //   ellipse(mouseX, mouseY, size, size);
+  // }
+  else{
+    ellipse(mouseX, mouseY, size, size);
+  }
+}
+// const eraser = document.getElementById("pen-eraser")
+// eraser.addEventListener('click',(e)=>{
+//   // console.log("yes")
+//   // fill(color);
+//   stroke(color);
+//   ellipse(mouseX, mouseY, size, size);
+// })
+_("#reset-canvas").addEventListener("click", function(){
+ window.document.reload(); 
+});
+_("#save-canvas").addEventListener("click",function(){
+  saveCanvas(canvas, "sketch", "png");
+});
+let colorpicker=document.querySelector("#base");
+colorpicker.addEventListener('change', (event) => {
+document.body.style.backgroundColor = event.target.value;
+});
+
+function white(){
+  fill(255);
+}
+function defaultbg() {
+document.body.style.backgroundColor = "white";
+}
+function myFunction1() {
+  document.body.style.backgroundColor = "red";
+}
+function myFunction2() {
+  document.body.style.backgroundColor = "orange";
+}
+function myFunction3() {
+  document.body.style.backgroundColor = "yellow";
+}
+function myFunction4() {
+  document.body.style.backgroundColor = "blue";
+}
+function myFunction5() {
+  document.body.style.backgroundColor = "green";
+}
+function myFunction6() {
+  document.body.style.backgroundColor = "purple";
+}
+function myFunction7() {
+  document.body.style.backgroundColor = "black";
+}
